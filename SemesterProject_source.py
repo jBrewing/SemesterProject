e43f2fd8cf940f0fe5f8569e539c30f3e06350d1
jBrewing <jbrewer256@gmail.com>
2018-11-14 14:30:13 -0700

    Initial commit

---
+++ SemesterProject_source.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------------
+# SemesterProject_source.py
+#
+# Author(s):  Joseph Brewer / Jaxon White
+# Creation Date: 11/14/2018
+#
+# Development Environment:
+#   Pycharm
+#   Python 3.7
+#
+# This script is a general visualization tool used in conjunction with
+# the ongoing water-related-energy study, under the CUASHI research
+# umbrella, focusing on the Living & Learning Community at Utah State.
+# Based on input, it queries an InfluxDB database, where
+# water consumption data and associated water temperature data are stored.
+# Based on user input, the script performs a variety of tasks, including:
+#   - comparison of water-related energy use between buildings
+#   - outputs a variety of visualizations for water-related energy use
+#   - performs some basic statistical analysis on water-related energy use.
+#
+# ----------------------------------------------------------------------------
+
+#commit
